688d61ed799a10062408fddd40a35621c4f8dc41
RishiReddyR <rishirreddy@gmail.com>
2021-08-02 19:42:07 -0400

    First push

--- App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {Button, NativeModules, View, NativeAppEventEmitter} from 'react-native';
-import BleManager from 'react-native-ble-manager';
-import Scan from './app/Scan';
-
-export default function FirstApp() {
-  return <Scan />;
-}
